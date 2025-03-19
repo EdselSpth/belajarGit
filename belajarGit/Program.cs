95943473696519aa6cf2a0cea5d29d305921318b
edselspth <edselspth@gmail.com>
2025-03-19 14:26:49 +0700

    tes

--- belajarGit/Program.cs
+++ belajarGit/Program.cs
@@ -1,2 +1,9 @@
-﻿Console.WriteLine("tes");
-Console.WriteLine("tes");
+﻿
+public class Program
+{
+    //dari branch main
+    public static void Main()
+    {
+        Console.WriteLine("Halo Dunia")
+    }
+}
